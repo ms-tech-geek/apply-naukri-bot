b308bc3e0b9407fe34bf9416a2367bcf9a01838e
Mayank Sethi <mayank@digibrain.co.in>
2024-06-19 11:43:56 +0530

    save fetches job in a file

--- cypress/e2e/applyJobs.js
+++ cypress/e2e/applyJobs.js
@@ -1,6 +1,6 @@
-// cypress/e2e/applyJobs.js
+const fs = require('fs');
 
-describe('Naukri Job Application Automation', () => {
+describe('Fetch Job Links from Naukri', () => {
   const username = 'thecorporator@gmail.com'; // Your naukri.com email
   const password = 'D3f@u!0r'; // Your naukri.com password
   const keywords = 'react developer';
@@ -9,21 +9,21 @@ describe('Naukri Job Application Automation', () => {
   before(() => {
     // Catch and ignore uncaught exceptions
     Cypress.on('uncaught:exception', (err, runnable) => {
-      // Returning false here prevents Cypress from failing the test
       return false;
     });
+
+    // Increase default timeout
+    Cypress.config('defaultCommandTimeout', 60000);
+    Cypress.config('pageLoadTimeout', 60000);
   });
 
-  it('logs in and applies for jobs', () => {
+  it('logs in to Naukri and performs a job search', () => {
     // Intercept network requests
     cy.intercept('GET', '**/').as('naukriHome');
 
     // Visit Naukri.com homepage
     cy.visit('https://www.naukri.com');
     cy.wait('@naukriHome');
-
-    // Log URL to confirm
-    cy.url().should('include', 'naukri.com');
     cy.log('Visited Naukri Homepage');
 
     // Click on the login button
@@ -44,12 +44,12 @@ describe('Naukri Job Application Automation', () => {
     cy.get('button[type="submit"]').click();
     cy.log('Submitted Login Form');
 
-    // Wait for 10 seconds for login to complete
-    cy.wait(10000);
+    // Wait for login to complete
+    cy.wait(3000);
 
     // Perform job search
     cy.get('.nI-gNb-sb__main').click();
-    cy.log('Click on Search Bar');
+    cy.log('Clicked on Search Bar');
 
     // Wait for 3 seconds
     cy.wait(3000);
@@ -73,7 +73,7 @@ describe('Naukri Job Application Automation', () => {
     cy.wait(5000);
     cy.log('Performed Job Search');
 
-    // Extract job links
+    // Extract job links and save to JSON file
     const jobLinks = [];
     cy.get('[class="srp-jobtuple-wrapper"] a')
       .each(($el) => {
@@ -81,29 +81,12 @@ describe('Naukri Job Application Automation', () => {
         if (link && !link.startsWith('http')) {
           link = `https://www.naukri.com${link}`;
         }
+        cy.log('Extracted link: ' + link);
         jobLinks.push(link);
       })
       .then(() => {
-        cy.wrap(jobLinks).each((link) => {
-          if (link) {
-            cy.visit(link);
-            cy.log('Visited Job Link: ' + link);
-
-            // Wait for the apply button to be visible
-            cy.contains('Apply', { timeout: 10000 })
-              .should('be.visible')
-              .click();
-            cy.log('Clicked Apply');
-
-            // Wait for the apply process to complete
-            cy.wait(2000);
-            cy.log('Applied for Job');
-
-            // Go back to the search results
-            cy.go('back');
-            cy.wait(3000); // Wait for the search results page to load
-          }
-        });
+        cy.writeFile('cypress/fixtures/jobLinks.json', jobLinks);
+        cy.log('Saved job links to jobLinks.json');
       });
   });
 });
